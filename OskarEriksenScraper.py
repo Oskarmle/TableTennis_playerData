2606f49d27bd07b6a5dd2e8b482aec39974effed
Oskarmle <121121527+Oskarmle@users.noreply.github.com>
2024-10-26 23:57:40 +0200

    updated python script to run through all players

--- OskarEriksenScraper.py
+++ OskarEriksenScraper.py
@@ -7,7 +7,6 @@ from bs4 import BeautifulSoup
 import time
 import os
 from dotenv import load_dotenv
-import datetime  # For generating unique timestamps
 from pymongo import MongoClient
 
 # Load environment variables from .env file
@@ -20,36 +19,37 @@ password = os.getenv('MONGODB_PASS')
 connection_string = f"mongodb+srv://{username}:{password}@tabletennis0.u10qa.mongodb.net/?retryWrites=true&w=majority"
 client = MongoClient(connection_string)
 
-#############################################################################################
-#############################################################################################
-#################### REMEMBER TO CHANGE URL ON LINE 34 AND ID ON LINE 104 ####################
-#############################################################################################
-#############################################################################################
+# List of player information containing ID and URL
+players = [
+    {"id": "PE", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#305214,59,7467481,42024"},
+    {"id": "BH", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#343985,59,7467244,42024"},
+    {"id": "CN", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#316342,59,7482751,42024"},
+    {"id": "DS", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#142898,59,7452335,42024"},
+    {"id": "FB", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#328783,59,7465770,42024"},
+    {"id": "HP", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#343754,59,7478937,42024"},
+    {"id": "HW", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#141533,59,7449840,42024"},
+    {"id": "JOJ", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#340787,59,7478179,42024"},
+    {"id": "JR", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#342680,59,7466419,42024"},
+    {"id": "KS", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#343104,59,7477982,42024"},
+    {"id": "MB", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#151637,59,7470091,42024"},
+    {"id": "MR", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#319070,59,7457754,42024"},
+    {"id": "NK", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#317843,59,7481672,42024"},
+    {"id": "NZ", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#346720,59,7478163,42024"},
+    {"id": "OMLE", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#319068,59,7457598,42024"},
+    {"id": "SM", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#329787,59,7478266,42024"},
+    {"id": "SN", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#326538,59,7470020,42024"},
+    {"id": "TD", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#326539,59,7459788,42024"},
+    {"id": "TL", "url": "https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#145074,59,7452597,42024"},
 
-def scrapPlayer():
+    # Add other players here
+]
+
+def scrapPlayer(player):
     # Set up Selenium WebDriver (Chrome in this case)
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
-    ##################### REMEMBER TO CHANGE URL ####################
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#305214,59,7467481,42024") # Pernille Eskebo | PE
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#343985,59,7467244,42024") # Birk Herman | BH
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#316342,59,7482751,42024") # Carsten Nielsen | CN
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#142898,59,7452335,42024") # Daniel Strauss | DS
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#328783,59,7465770,42024") # Flemming Baarts | FB
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#343754,59,7478937,42024") # Hossein Pakdast | HP
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#141533,59,7449840,42024") # Heidi Weihrauch | HW
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#340787,59,7478179,42024") # John O. Jensen | JOJ
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#342680,59,7466419,42024") # Jesper Rasmussen | JR
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#343104,59,7477982,42024") # Karsten Skals | KS
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#151637,59,7470091,42024") # Mogens Bentzen | MB
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#319070,59,7457754,42024") # Morten Ravn | MR
-    driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#317843,59,7481672,42024") # Nicolai Kistrup | NK
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#346720,59,7478163,42024") # Nikolaj Zingenberg | NZ
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#319068,59,7457598,42024") # Oskar Eriksen | OMLE
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#329787,59,7478266,42024") # Subhayu Mukherjee | SM
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#326538,59,7470020,42024") # Stig Nørgaard | SN
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#326539,59,7459788,42024") # Torri Danekilde | TD
-    # driver.get("https://www.bordtennisportalen.dk/DBTU/Spiller/VisSpiller/#145074,59,7452597,42024") # Torben Lang | TL
+    # Load the player's URL
+    driver.get(player["url"])
 
     # Wait for the page to fully load
     time.sleep(5)  # Adjust based on page loading time
@@ -98,25 +98,7 @@ def scrapPlayer():
 
             # Create a new entry
             new_entry = {
-            # "id": "PE",   #################### REMEMBER TO CHANGE ID ####################
-            # "id": "BH",   #################### REMEMBER TO CHANGE ID ####################
-            # "id": "CN",   #################### REMEMBER TO CHANGE ID ####################
-            # "id": "DS",   #################### REMEMBER TO CHANGE ID ####################
-            # "id": "FB",   #################### REMEMBER TO CHANGE ID ####################
-            # "id": "HP",   #################### REMEMBER TO CHANGE ID ####################
-            # "id": "HW",   #################### REMEMBER TO CHANGE ID ####################
-            # "id": "JOJ",  #################### REMEMBER TO CHANGE ID ####################
-            # "id": "JR",   #################### REMEMBER TO CHANGE ID ####################
-            # "id": "KS",   #################### REMEMBER TO CHANGE ID ####################
-            # "id": "MB",   #################### REMEMBER TO CHANGE ID ####################
-            # "id": "MR",   #################### REMEMBER TO CHANGE ID ####################
-            "id": "NK",   #################### REMEMBER TO CHANGE ID ####################
-            # "id": "NZ",   #################### REMEMBER TO CHANGE ID ####################
-            # "id": "OMLE", #################### REMEMBER TO CHANGE ID ####################
-            # "id": "SM",   #################### REMEMBER TO CHANGE ID ####################
-            # "id": "SN",   #################### REMEMBER TO CHANGE ID ####################
-            # "id": "TD",   #################### REMEMBER TO CHANGE ID ####################
-            # "id": "TL",   #################### REMEMBER TO CHANGE ID ####################
+                "id": player["id"],
                 "date": last_date,
                 "tournament": last_tournament_name,
                 "player": {
@@ -132,36 +114,15 @@ def scrapPlayer():
     # Close the browser
     driver.quit()
 
-    
-    # specific_id = "PE"   #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "BH"   #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "CN"   #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "DS"   #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "FB"   #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "HP"   #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "HW"   #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "JOJ"  #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "JR"   #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "KS"   #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "MB"   #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "MR"   #################### REMEMBER TO CHANGE ID ####################
-    specific_id = "NK"   #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "NZ"   #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "OMLE" #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "SM"   #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "SN"   #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "TD"   #################### REMEMBER TO CHANGE ID ####################
-    # specific_id = "TL"   #################### REMEMBER TO CHANGE ID ####################
-
     # Insert data into MongoDB
-    insert_to_mongodb(new_data, specific_id)
+    insert_to_mongodb(new_data, player["id"])
 
 def insert_to_mongodb(data, specific_id):
     db = client["tabletennis"]  # Database name
     collection = db["players"]  # Collection 
 
     # Count existing records in the database with the specified ID
-    existing_count = collection.count_documents({"id": specific_id})  # Count records with ID 'OMLE'
+    existing_count = collection.count_documents({"id": specific_id})  # Count records with ID
     print(f"Existing records in the database for ID '{specific_id}': {existing_count}")
 
     # Filter the new data for the specific ID
@@ -193,8 +154,6 @@ def insert_to_mongodb(data, specific_id):
 
     print(f"Inserted {inserted_count} new records into MongoDB for ID '{specific_id}'.")
 
-
-
-
-# Call the scraping function
-scrapPlayer()
+# Call the scraping function for each player
+for player in players:
+    scrapPlayer(player)
